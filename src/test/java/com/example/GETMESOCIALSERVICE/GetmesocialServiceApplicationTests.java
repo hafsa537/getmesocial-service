4e20d3887cb09937635f005afb826b2c7ca3ca6c
hafsa537 <hafsa.star@gmail.com>
2021-01-24 22:16:08 +0530

    first commit

---
+++ src/test/java/com/example/GETMESOCIALSERVICE/GetmesocialServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GETMESOCIALSERVICE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetmesocialServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
