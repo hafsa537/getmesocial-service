e6a0af7e0406faec73c6d669aea3b1977da8acab
hafsa537 <hafsa.star@gmail.com>
2021-01-28 11:40:17 +0530

    test

--- src/main/java/com/example/GETMESOCIALSERVICE/repository/UserRepository.java
+++ src/main/java/com/example/GETMESOCIALSERVICE/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.GETMESOCIALSERVICE.repository;
 
 import com.example.GETMESOCIALSERVICE.Model.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UserRepository {
     public User getUser() {
         User user = new User("Hafsa", "Hyderabad", 30, "C:\\Users\\syeda hafsa fatima\\Desktop\\MY phone backuup\\DCIM\\Camera\\ProfilePic.jpg");
