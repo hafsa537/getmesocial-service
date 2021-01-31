1da276c34da8a239aa4aa170a4c167dca9f232f1
hafsa537 <hafsa.star@gmail.com>
2021-01-31 14:41:27 +0530

    testcommitchange

--- src/main/java/com/example/GETMESOCIALSERVICE/repository/UserRepository.java
+++ src/main/java/com/example/GETMESOCIALSERVICE/repository/UserRepository.java
@@ -13,7 +13,7 @@ public class UserRepository {
     List<User> userList=new ArrayList();
     public User getUser() {
 
-        User user = new User("Hafsa", "Hyderabad", 30 , "C:\\Users\\syeda hafsa fatima\\Desktop\\MY phone backuup\\DCIM\\Camera\\ProfilePic.jpg");
+        User user = new User("Hafsa" , "Hyderabad", 30 , "C:\\Users\\syeda hafsa fatima\\Desktop\\MY phone backuup\\DCIM\\Camera\\ProfilePic.jpg");
         return user;
     }
 
